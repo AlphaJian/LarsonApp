395a47c7024ac1a5d431e2ca01543e8a4491e915
Perry <chenzhipeng901105@gmail.com>
2016-11-08 10:47:42 +0800

    add cell click block

--- LarsonApp/LarsonApp/Class/Appointment/Views/Cell/CompleteAppointmentTableViewCell.swift
+++ LarsonApp/LarsonApp/Class/Appointment/Views/Cell/CompleteAppointmentTableViewCell.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class CompleteAppointmentTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var placeholderLabel: UILabel!
+    @IBOutlet weak var legacyIdLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.containerView.layer.cornerRadius = 4.0
+        self.containerView.layer.shouldRasterize = true
         // Initialization code
     }
+    
+    func setupCellData(model: AppointmentModel) {
+        self.placeholderLabel.text = model.jobType
+        self.legacyIdLabel.text = model.legacyId
+        self.nameLabel.text = model.customerName
+    }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
