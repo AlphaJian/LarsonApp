ae05b8081a64ab158659def29c22d84af3e32054
Perry <chenzhipeng901105@gmail.com>
2016-11-09 16:55:49 +0800

    add sort appointment list

--- LarsonApp/LarsonApp/Class/Appointment/AppointmentViewController.swift
+++ LarsonApp/LarsonApp/Class/Appointment/AppointmentViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import Firebase
 
 class AppointmentViewController: BaseViewController {
     var tableview : AppointmentsTableView!
     
     var listBlock : ReturnBlock?
+    
+    private lazy var appointmentListRef : FIRDatabaseReference = FIRDatabase.database().reference().child("engineerApp").child("engineers-appointments").child(DataManager.shareManager.referenceStr)
+    private var appointmentListRefHandle: FIRDatabaseHandle?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -24,41 +28,97 @@ class AppointmentViewController: BaseViewController {
         initNavView(title: "Appointment List")
         initUI()
         self.view.showhud()
-        DataManager.shareManager.fetchAppointList(successHandler: { (obj) in
-            
-            DispatchQueue.main.async {
-                var arrNew = [AppointmentModel]()
-                if self.listBlock != nil {
-                    let arrayList = obj as! [AppointmentModel]
-                    arrNew = arrayList.filter({ (model) -> Bool in
+        
+        appointmentListRef.observe(.value, with: { (snapshot) in
+            self.view.hidehud()
+            if self.tableview != nil {
+                var inProgressIndex = 0
+                var completeIndex = 0
+                
+                let dic = snapshot.value as? NSDictionary
+                var arr = [AppointmentModel]()
+                for i in 0...(dic?.allValues.count)! - 1
+                {
+                    let tempDic = dic?.allValues[i] as? NSDictionary
+                    let model = AppointmentModel()
+                    //                model.initWithDic(dic: tempDic!)
+                    model.parseDicToSelf(dic: tempDic!)
+                    
+                    if model.currentStatus.lowercased() == "new" {
+                        arr.insert(model, at: 0)
+                        inProgressIndex += 1
+                        completeIndex += 1
+                    } else if model.currentStatus.lowercased() == "in progress" {
+                        arr.insert(model, at: inProgressIndex)
+                        inProgressIndex += 1
+                        completeIndex += 1
+                    } else if model.currentStatus.lowercased() == "completed" {
+                        arr.insert(model, at: completeIndex)
+                        completeIndex += 1
+                    }
+                }
+
+                DispatchQueue.main.async {
+                    var arrNew = arr.filter({ (model) -> Bool in
                         model.currentStatus.lowercased() == "new"
                     })
-                    if arrNew.count > 0 {
-                        self.listBlock!(arrNew[0] as AnyObject)
-                    } else {
-                        self.listBlock!(AppointmentModel() as AnyObject)
+                    if self.listBlock != nil {
+                        if arrNew.count > 0 {
+                            self.listBlock!(arrNew[0] as AnyObject)
+                        } else {
+                            self.listBlock!(AppointmentModel() as AnyObject)
+                        }
                     }
-                    
+                    self.view.hidehud()
+                    self.tableview.items = arr
+                    self.tableview.reloadData()
                 }
-                self.view.hidehud()
-                self.tableview.items = obj as! [AppointmentModel]
-                self.tableview.reloadData()
+
             }
-            }) { (obj) in
-                self.view.hidehud()
-                print(obj)
-        }
+        }, withCancel: { (error) in
+            self.view.hidehud()
+            print(error)
+        })
+        
+//        DataManager.shareManager.fetchAppointList(successHandler: { (obj) in
+//            
+//            DispatchQueue.main.async {
+//                var arrNew = [AppointmentModel]()
+//                if self.listBlock != nil {
+//                    let arrayList = obj as! [AppointmentModel]
+//                    arrNew = arrayList.filter({ (model) -> Bool in
+//                        model.currentStatus.lowercased() == "new"
+//                    })
+//                    if arrNew.count > 0 {
+//                        self.listBlock!(arrNew[0] as AnyObject)
+//                    } else {
+//                        self.listBlock!(AppointmentModel() as AnyObject)
+//                    }
+//                    
+//                }
+//                self.view.hidehud()
+//                self.tableview.items = obj as! [AppointmentModel]
+//                self.tableview.reloadData()
+//            }
+//            }) { (obj) in
+//                self.view.hidehud()
+//                print(obj)
+//        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
+    }
+    
+    deinit {
+        print("#### deinit ####")
+        appointmentListRef.removeAllObservers()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        
+//
     }
 
     func initUI(){
